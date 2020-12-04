35374f103a24e92c3af44ada7b415b9236909bf4
Tyler Lesmann <redhatcat@gmail.com>
2020-12-04 16:36:32 -0700

    Handle EXIF rotation

--- app/src/main/java/me/echodev/resizer/util/ImageUtils.java
+++ app/src/main/java/me/echodev/resizer/util/ImageUtils.java
@@ -2,6 +2,8 @@ package me.echodev.resizer.util;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
 
 import java.io.File;
 import java.io.IOException;
@@ -11,6 +13,13 @@ import java.io.IOException;
  */
 
 public class ImageUtils {
+    private static int exifToDegrees(int exifOrientation) {
+        if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_90) { return 90; }
+        else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_180) {  return 180; }
+        else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_270) {  return 270; }
+        return 0;
+    }
+
     public static File getScaledImage(int targetLength, int quality, Bitmap.CompressFormat compressFormat,
             String outputDirPath, String outputFilename, File sourceImage) throws IOException {
         File directory = new File(outputDirPath);
@@ -33,6 +42,19 @@ public class ImageUtils {
         options.inJustDecodeBounds = false;
         Bitmap bitmap = BitmapFactory.decodeFile(sourceImage.getAbsolutePath(), options);
 
+        // Process EXIF information
+        int rotationInDegrees = 0;
+        Matrix matrix = new Matrix();
+        try {
+          ExifInterface exif = new ExifInterface(sourceImage.getAbsolutePath());
+          int rotation = exif.getAttributeInt(
+            ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+          rotationInDegrees = exifToDegrees(rotation);
+          if (rotation != 0) {matrix.preRotate(rotationInDegrees);}
+        } catch (IOException e) {
+          // Silently fail
+        }
+
         // Get the dimensions of the original bitmap
         int originalWidth = options.outWidth;
         int originalHeight = options.outHeight;
@@ -50,6 +72,13 @@ public class ImageUtils {
             targetWidth = Math.round(targetHeight / aspectRatio);
         }
 
-        return Bitmap.createScaledBitmap(bitmap, targetWidth, targetHeight, true);
+        Bitmap rotated = Bitmap.createBitmap(
+          bitmap, 0, 0, originalWidth, originalHeight, matrix, true);
+
+        // Swap height and width if rotated
+        if (rotationInDegrees == 90 || rotationInDegrees == 270) {
+          targetHeight = targetHeight ^ targetWidth ^ (targetWidth = targetHeight);
+        }
+        return Bitmap.createScaledBitmap(rotated, targetWidth, targetHeight, true);
     }
 }
